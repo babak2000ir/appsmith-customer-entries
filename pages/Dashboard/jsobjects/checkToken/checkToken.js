6066f0c8ab932ed864fc611ce3ed4815ca340083
babak2000ir <babak2000ir>
2025-04-06 11:04:50 +0000

    added dashboard

--- pages/Dashboard/jsobjects/checkToken/checkToken.js
+++ pages/Dashboard/jsobjects/checkToken/checkToken.js
@@ -2,13 +2,18 @@ export default {
 	async checkToken () {
 		return verify_token.run()
 			.then(data => {
-			if (data.value)
-				navigateTo('Dashboard')
+			if (!data.value)
+				navigateTo('Login')
+			else {
+				Spinner.setVisibility(false);
+				DashboardWidget.setVisibility(true);
+			}
 		})
 			.catch(() => {
 			storeValue("token", '');
 			storeValue("userId", '');
 			storeValue("userName", '');
+			navigateTo('Login');
 		})
 	}
 }
